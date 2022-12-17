81239819704376c55875695f3e679680f3a718b1
syalanuj <syal.anuj@gmail.com>
2022-12-17 13:18:12 +0530

    added new feature bye with datetime name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 from flask import Flask
+from datetime import datetime
 app = Flask(__name__)
  
 @app.route('/hello/<name>')
@@ -9,6 +10,10 @@ def hello_name(name):
 @app.route('/bye/<name>')
 def bye_name(name):
    return 'Bye %s!' % name
- 
+
+@app.route('/bye_with_datetime_name/<name>')
+def bye_with_datetime_name(name):
+   return f"Bye {name} current time is {datetime.now()}"
+
 if __name__ == '__main__':
    app.run()
