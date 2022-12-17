14d5dad35508c61e7604c7225d8c4741a7558f4c
syalanuj <syal.anuj@gmail.com>
2022-12-17 13:11:07 +0530

    init feature datetime route with hello

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 from flask import Flask
+from datetime import datetime
 app = Flask(__name__)
  
 @app.route('/hello/<name>')
@@ -9,6 +10,10 @@ def hello_name(name):
 @app.route('/bye/<name>')
 def bye_name(name):
    return 'Bye %s!' % name
+
+@app.route('/hello_with_datetime/<name>')
+def hello_name(name):
+   return f'Hello {name}! thank you for checking us out at {datetime.now()}'
  
 if __name__ == '__main__':
    app.run()
