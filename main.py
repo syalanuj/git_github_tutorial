256f26c23949587a8c8e10d9517f0d1e62092558
syalanuj <syal.anuj@gmail.com>
2022-12-17 12:49:00 +0530

    added a new bye route

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 @app.route('/hello/<name>')
 def hello_name(name):
    return 'Hello %s!' % name
+
+@app.route('/bye/<name>')
+def bye_name(name):
+   return 'Bye %s!' % name
  
 if __name__ == '__main__':
    app.run()
